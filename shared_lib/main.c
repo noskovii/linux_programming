85fbd86582f859c25f5b9b394db5322e89c4e230
Ilya Noskov <noskovii@mail.ru>
2023-04-10 20:50:24 +0300

    Add shared lib

---
+++ shared_lib/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "solution.c"
+
+
+int main(int argc, char *argv[]) {
+    int num = 1;
+    int res = stringStat("hello", 10, &num);
+    printf("res = %d num = %d\n", res, num);
+}
+
---
+++ shared_lib/solution.h
@@ -0,0 +1,1 @@
+int stringStat(const char *string, size_t multiplier, int *count);
