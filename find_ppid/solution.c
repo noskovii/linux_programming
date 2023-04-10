a29417be9c7a919fdd70c2bacb7f9cce2f7697b6
Ilya Noskov <noskovii@mail.ru>
2023-04-10 21:22:20 +0300

    Add find ppid

---
+++ find_ppid/solution.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int main()
+{
+  int pid;
+  int cur_pid = getpid();
+  int parent_pid = getppid();
+
+  char str[100];
+  sprintf(str, "/proc/%d/stat", cur_pid);
+
+  FILE *fd = fopen(str, "r");
+
+  int tmp_int;
+  char tmp_buf[100];
+  char tmp_chr;
+  int ppid;
+
+  fscanf(fd, "%d %s %c %d", &tmp_int, tmp_buf, &tmp_chr, &ppid);
+
+  printf("%d\n", ppid);
+}
+
